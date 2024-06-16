b761510d3315c04517ad48a6c40290c68cbe4a8c
Marko <Marko@DESKTOP-RS86C8A>
2024-06-15 23:33:56 -0500

    Codigo de la conexion

--- src/main/java/com/restaurante/modelo/Conexion.java
+++ src/main/java/com/restaurante/modelo/Conexion.java
@@ -4,10 +4,22 @@
  */
 package com.restaurante.modelo;
 
-/**
- *
- * @author Marko
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
-    
+
+    Connection con;
+
+    public Connection getConnection(){
+        try {
+            String myBD = "jdbc:mysql://localhost:3306/restaurante?serverTimezone=America/Los_Angeles";
+            con = DriverManager.getConnection(myBD, "root", "123456");
+            return con;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return null;
+    }
 }
