10752be3913b063ab670c464b4de6cf469301a78
Marko <Marko@DESKTOP-RS86C8A>
2024-06-15 22:33:02 -0500

    Carga inicial

---
+++ src/main/java/com/restautante/restaurantedelicia/RestauranteDelicia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.restautante.restaurantedelicia;
+
+/**
+ *
+ * @author Marko
+ */
+public class RestauranteDelicia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
